f88d8e7d591ec14fcba6052a2da77892616d22f7
Banopartt <turgaliev.almat11@gmail.com>
2025-03-30 11:01:14 +0600

    added sidebar

--- src/components/widgets/sidebar-widget.vue
+++ src/components/widgets/sidebar-widget.vue
@@ -1,11 +1,33 @@
 <template>
-<aside>
-    
+<aside class="bg-gray-100 z-50 sticky h-0 opactiy-0 transition-all px-4 py-8 duration-700 overflow-x-hidden invisible"
+:class="{
+'opactiy-100 visible h-svh': isOpenSidebar
+}"
+>
+<nav>
+    <ul>
+        <li v-for="(link, id) in navbarLinks" class="text-xl font-rubik font-medium text-customBlack" :key="'sidebarLinks' + id">
+            <router-link :to="link.path">{{ link.name }}</router-link>
+
+        </li>
+    </ul>
+    <ul>
+        <li v-for="(link, id) in navbarModals.filter((link) => link.name !== 'Войти')" class="text-xl font-rubik font-medium text-customBlack" :key="'sidebarLinks' + id">
+            <router-link :to="link.path">{{ link.name }}</router-link>
+
+        </li>
+    </ul>
+</nav>
+<hr class="text-[#C2CDCE]">
 </aside>
 </template>
 
 <script setup>
+import { navbarLinks, navbarModals } from '@/const/links';
+import { useSidebarStore } from '@/stores/sidebar';
+import { storeToRefs } from 'pinia';
 
+const {isOpenSidebar} = storeToRefs(useSidebarStore())
 </script>
 
 <style scoped>
---
+++ src/const/links.js
@@ -0,0 +1,26 @@
+export const navbarLinks = [
+    {
+      name: 'Вакансии',
+      path: '/',
+    },
+    {
+      name: 'Резюме',
+      path: '/',
+    },
+    {
+      name: 'Цены и услуги',
+      path: '/',
+    },
+  ]
+
+export const navbarModals = [
+    {
+      name: 'Резюме',
+    },
+    {
+      name: 'Вакансия',
+    },
+    {
+      name: 'Войти',
+    },
+  ]
