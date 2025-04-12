6d95253e631d95f1077b45c55d81aa3c36e26467
Banopartt <turgaliev.almat11@gmail.com>
2025-04-12 11:04:49 +0600

    added search

--- src/components/search-vacansies.vue
+++ src/components/search-vacansies.vue
@@ -1,16 +1,33 @@
 <template>
     <div class="container mx-auto px-5">
-        <div class="bg-white p-4 flex items-center gap-4">
-            <InputText placeholder="Поиск по вакансиям" class="w-full " />
-            <Button class="w-36">Все вакансии</Button>
-            <Button class="w-32">Все резюме</Button>
+        <div class="bg-white p-4 flex items-center gap-1">
+            <div class="relative w-full">
+                <i v-show="isVisibleSearchIcon" class="pi pi-search absolute ml-3 mt-1 !text-2xl text-gray-500 "></i>
+                <InputText v-model="searchValue" placeholder="Поиск по вакансиям" class="w-11/12 !bg-lightGrey !pl-12 !border-none !text-customGray " />
+            </div>
+            <Button class="w-52 !text-customPink !border-customPink !bg-transparent 2xl:w-36 ">Все вакансии</Button>
+            <Button class="w-52 !text-black !border-black !bg-transparent 2xl:w-36">Все резюме</Button>
         </div>
     </div>
 </template>
 
 <script setup>
 import { InputText, Button } from 'primevue';
+import { ref, watch } from 'vue';
 
+
+const searchValue = ref('')
+
+const isVisibleSearchIcon = ref(true)
+
+watch(searchValue, ()=>{
+    if (searchValue.value.length) {
+        isVisibleSearchIcon.value = false
+    }
+    if (searchValue.value.length === 0) {
+        isVisibleSearchIcon.value = true
+    }
+})
 
 </script>
 
