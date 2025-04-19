762e271da211727eaa7f7b502233f5cf00b54322
Banopartt <turgaliev.almat11@gmail.com>
2025-04-19 19:09:32 +0600

    added departments-card

--- src/router/index.js
+++ src/router/index.js
@@ -17,6 +17,11 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue'),
     },
+    {
+      path: '/vacansies',
+      name:'vacansies',
+      component: () => import('../views/VacansyView.vue') 
+    }
   ],
 })
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,6 +3,8 @@ import BannerInfo from '@/components/banner-info.vue';
 import SearchVacansies from '@/components/search-vacansies.vue';
 import CategoryCard from '@/components/widgets/category-card.vue';
 import { mockCategory } from '@/const/mock-category';
+import remoteDepartments from '@/components/remote-departments.vue';
+import { mockDepartments } from '@/const/mock-departments';
 
 
 </script>
@@ -15,7 +17,7 @@ import { mockCategory } from '@/const/mock-category';
     <div class="container mx-auto px-5 grid grid-cols-1 sm:grid-cols-2 gap-6">
       <CategoryCard v-for="category in mockCategory" :key="'mockCategory' + category.id" :category="category"/>
     </div>
-    <p class="container mx-auto px-5 mt-20 text-center font-light font-rubik text-customGray">
+    <p class="container mx-auto px-5 my-20 text-center font-light font-rubik text-customGray">
       Категория, Еще категория, Категория, Категория, Категория, Еще категория, Категория, Категория, <br> 
 Еще категория, Категория, Категория, Еще категория, Категория, Категория, Еще категория, Категория, Категория, 
 <br>
@@ -23,7 +25,14 @@ import { mockCategory } from '@/const/mock-category';
     </p>
    </section>
    <section class="bg-white">
-    
-   </section>
+  <div class="container mx-auto px-5 pt-20 ">
+    <h2 class="text-center text-customBlack font-rubik text-2xl">
+      Помогаем строить удаленные отделы
+    </h2>
+   <div class="mt-20 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6">
+    <remote-departments v-for="card in mockDepartments" :key="'mockDepartments' + card.id" :card="card" />
+   </div>
+  </div> 
+  </section>
   </div>
 </template>
