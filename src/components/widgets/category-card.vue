3e84bb6b2b03367a374f86cea30d8371cf5c0186
Banopartt <turgaliev.almat11@gmail.com>
2025-04-27 09:41:32 +0600

    added vacansy card

--- src/components/widgets/category-card.vue
+++ src/components/widgets/category-card.vue
@@ -11,22 +11,22 @@ const { category } = defineProps(['category'])
     <div class="bg-white flex justify-between py-8 px-7">
         <div class="font-rubik">
             <p class="text-customBlack font-semibold text-base xl:text-xl">
-                {{ category?.title || "Название не указано" }}
+                {{ category?.name || "Название не указано" }}
             </p>
             <span class="font-medium text-sm xl:text-base text-customBlack mt-5 block">
-                {{ category?.incomePrice ? `Средий доход ${category?.incomePrice} ₽` : 'Доход не указан' }}
+                {{ category?.incomeSalary ? `Средий доход ${category?.incomeSalary} ₽` : 'Доход не указан' }}
             </span>
             <span class="text-customGray mt-3 block text-sm xl:text-base">
-                {{ category?.responsesOfVacancy ? `${category?.responsesOfVacancy} отклика на вакансию` : 'Отклики не указаны' }}
+                {{ category?.responseOfVacansy ? `${category?.responseOfVacansy} отклика на вакансию` : 'Отклики не указаны' }}
             </span>
             <div class="flex items-center gap-4 mt-5">
                 <Button class="!border-none !bg-lightPink !text-customPink">Вакансии</Button>
                 <Button severity="contrast" class="!bg-lightGrey !text-customGray !border-none ">Резюме</Button>
             </div>
         </div>
-        <div class="hidden sm:block">
-            <img :src="category?.image" :alt="category.title" class="w-72" width="286" height="143">
-        </div>
+        <!-- <div class="hidden sm:block"> -->
+            <!-- <img :src="category?.image" :alt="category.title" class="w-72" width="286" height="143"> -->
+        <!-- </div> -->
     </div>
 </template>
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,10 +3,24 @@ import BannerInfo from '@/components/banner-info.vue';
 import SearchVacansies from '@/components/search-vacansies.vue';
 import CategoryCard from '@/components/widgets/category-card.vue';
 import { mockCategory } from '@/const/mock-category';
-import remoteDepartments from '@/components/remote-departments.vue';
-import { mockDepartments } from '@/const/mock-departments';
 import DepartmentSection from '@/components/department-section.vue';
+import { onMounted, ref } from 'vue';
+import {http} from '/api/http.js';
 
+const categories = ref([])
+
+async function fetchCategory() {
+  try{
+    const response = await http.get('/category')
+    categories.value = response.data
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+onMounted( ()=> {
+  fetchCategory()
+})
 
 </script>
 
@@ -16,7 +30,7 @@ import DepartmentSection from '@/components/department-section.vue';
    <SearchVacansies/>
    <section class="mt-10">
     <div class="container mx-auto px-5 grid grid-cols-1 sm:grid-cols-2 gap-6">
-      <CategoryCard v-for="category in mockCategory" :key="'mockCategory' + category.id" :category="category"/>
+      <CategoryCard v-for="category in categories" :key="'mockCategory' + category.id" :category="category"/>
     </div>
     <p class="container mx-auto px-5 my-20 text-center font-light font-rubik text-customGray">
       Категория, Еще категория, Категория, Категория, Категория, Еще категория, Категория, Категория, <br> 
