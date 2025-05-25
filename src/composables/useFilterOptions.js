46eefc852c6f746c9e44842a7236f3375783a809
Banopartt <turgaliev.almat11@gmail.com>
2025-05-25 11:01:49 +0600

    added filter categories

---
+++ src/composables/useFilterOptions.js
@@ -0,0 +1,49 @@
+import { http } from "@/api/http"
+import { ref } from "vue"
+
+export const useFilterOPtions = () => {
+    const category = ref(null)
+    const compensation = ref(null)
+    const level = ref(null)
+    const location = ref(null)
+    const isOptionsLoading = ref(false)
+
+    const filter = ref({
+        categories: [],
+        compensations: [],
+        levels: [],
+        locations: [],
+    })
+    const fetchCategories = async () => {
+        try {
+            isOptionsLoading.value = true
+            const data = (await http.get('/categories')).data
+            category.value = data.filter((item) => item.type === 'category')
+            compensation.value = data.filter((item) => item.type === 'compensation')
+            level.value = data.filter((item) => item.type === 'level')
+            location.value = data.filter((item) => item.type === 'location')
+        } catch (err) {
+            console.log(err)
+        } finally {
+            isOptionsLoading.value = false
+        }
+    }
+
+    const clearFilters = () => {
+        filter.value.categories = []
+        filter.value.compensations = []
+        filter.value.levels = []
+        filter.value.locations = []
+    }
+
+    return {
+        fetchCategories,
+        category,
+        compensation,
+        level,
+        location,
+        filter,
+        isOptionsLoading,
+        clearFilters
+    }
+}
